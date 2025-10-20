67cf867a51681a208935afe3e93fe2759265d67c
Iaas22 <ishaaamin22@gmail.com>
2025-10-20 16:00:48 +0200

    commit inicial

---
+++ src/main/java/com/ra2/mysql/MysqlApplication.java
@@ -0,0 +1,13 @@
+package com.ra2.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlApplication.class, args);
+	}
+
+}
